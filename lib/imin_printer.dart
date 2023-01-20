5ce55e5fb8416d308f88ad31951dff7e17a5acca
fuadreza <fuadreza1011@gmail.com>
2023-01-20 22:57:16 +0700

    add description on the functions

--- lib/imin_printer.dart
+++ lib/imin_printer.dart
@@ -11,34 +11,65 @@ class IminPrinter {
     return IminPrinterPlatform.instance.getPlatformVersion();
   }
 
+  /// Initialize Imin Printer
+  ///
+  /// Option to Set paper size with [printSizeImin] with
+  /// the default value is for 58mm paper.
   Future<void> initPrinter({PrintSizeImin printSizeImin = PrintSizeImin.mm58}) async {
     IminPrinterPlatform.instance.initPrinter(printSizeImin);
   }
 
+  /// Get Status
+  ///
+  /// Return [String] printer status. Different printer
+  /// gives different status code.
+  /// Please refer to this site:
+  /// https://oss-sg.imin.sg/docs/en/PrinterSDK.html
   Future<String> getStatus() async {
     return IminPrinterPlatform.instance.getStatus();
   }
 
+  /// Print raw bytes
+  ///
+  /// Accept [bytes] with type [Uint8List] and send
+  /// that raw bytes to printer.
   Future<void> printBytes(Uint8List bytes) async {
     IminPrinterPlatform.instance.printBytes(bytes);
   }
 
+  /// Print Text
+  ///
+  /// Add custom style using [PrintStyle] with values.
+  /// like alignment, text size, and font style.
   Future<void> printText(String text, {PrintStyle printStyle = const PrintStyle()}) async {
     IminPrinterPlatform.instance.printText(text, printStyle);
   }
 
+  /// Print 2 Columns Text
+  ///
+  /// Accept [listText] with only 2 text [String].
+  /// Option to add custom style for text size.
   Future<void> print2ColumnsText(List<String> listText, {PrintStyle printStyle = const PrintStyle()}) async {
     IminPrinterPlatform.instance.print2ColumnsText(listText, printStyle);
   }
 
+  /// Print Image Bitmap
+  ///
+  /// Accept [Uint8List] bitmap wrap in [bytes].
   Future<void> printBitmap(Uint8List bytes) async {
     IminPrinterPlatform.instance.printBitmap(bytes);
   }
 
+  /// Print Image Base 64
+  ///
+  /// Accept [String] bitmap based on base64 format [base64].
   Future<void> printBitmapBase64(String base64) async {
     IminPrinterPlatform.instance.printBitmapBase64(base64);
   }
 
+  /// Set style for printer
+  ///
+  /// Set default style for printing using [PrintStyle].
   Future<void> setStyle(PrintStyle printStyle) async {
     IminPrinterPlatform.instance.setStyle(printStyle);
   }
